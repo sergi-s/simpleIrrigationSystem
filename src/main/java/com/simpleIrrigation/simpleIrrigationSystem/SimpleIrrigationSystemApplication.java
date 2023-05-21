9d37e8c9a46a7e91bd84da76d718072a0e2a5375
sergi-ss <sergi@thepalace.dev>
2023-05-21 19:03:00 +0300

    init commit

---
+++ src/main/java/com/simpleIrrigation/simpleIrrigationSystem/SimpleIrrigationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.simpleIrrigation.simpleIrrigationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIrrigationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleIrrigationSystemApplication.class, args);
+	}
+
+}
